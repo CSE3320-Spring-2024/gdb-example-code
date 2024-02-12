deec36ae6faeaf7e86b8dd510ad2de1239a80fcc
Trevor Bakker <tbakker@gmail.com>
2024-02-12 22:59:43 +0000

    added day 3

---
+++ day_3/main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+/*  
+ *    Use gdb to trace a proceblem with a child process
+ *
+ *    set follow-exec-mode child
+ *
+ *
+ *
+ *
+ */
+
+char * token[10];
+
+int main( int argc, char * argv[] )
+{
+  pid_t pid = fork();
+
+  if( pid == 0 )
+  {
+     if( strcmp( token[0], "exit" ) == 0 )
+     {
+        printf("Found one\n");
+     }  
+     else
+     {
+        printf("Didn't find one\n");
+     }
+     
+     printf("Done searching\n");
+  }
+  else
+  {
+    int status;
+    wait( &status );
+  }
+}
